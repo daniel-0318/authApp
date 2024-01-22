ac12c2604daf8a2f925ef9718f13e171199c82c0
Daniel Ramirez <dani_0318@hotmail.com>
2024-01-22 18:21:13 -0500

    guards implementado para proteger el dashboard 366

--- src/app/dashboard/pages/dashboardLayout/dashboardLayout.component.ts
+++ src/app/dashboard/pages/dashboardLayout/dashboardLayout.component.ts
@@ -1,8 +1,19 @@
 
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, computed, inject } from '@angular/core';
+import { AuthService } from '../../../auth/services/auth.service';
 
 @Component({
   templateUrl: './dashboardLayout.component.html',
   styleUrl: './dashboardLayout.component.scss',
 })
-export class DashboardLayoutComponent { }
+export class DashboardLayoutComponent {
+
+  private authService = inject( AuthService );
+
+  public user = computed( () => this.authService.currentUser() );
+
+  // get user(){
+  //   return this.authService.currentUser();
+  // }
+
+ }
